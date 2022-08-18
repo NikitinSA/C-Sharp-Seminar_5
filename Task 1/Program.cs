cf311a85334e119e1880df117110a964169805c4
Савелий <Savka9765@yandex.ru>
2022-08-18 20:26:57 +0300

    Программа, которая задает массив из 12 чисел и считает сумму положительных и отрицательных чисел

---
+++ Task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задать массив из 12 эллементов, заполненный случайными числами из промежутка [-9, 9].
+Найти сумма положительных и отрицательных элементов массива.*/
+
+int[] Get_array(int size, int min_Vale, int max_value)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(min_Vale, max_value + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+int[] array = Get_array (12, -9, 9); //задаем значения в функцию
+Console.WriteLine();
+int positive_sum = 0;
+int negative_sum = 0;
+foreach(int i in array)
+{
+    if (i > 0)
+    {
+        positive_sum += i;
+    }
+    else
+    {
+        negative_sum += i;
+    }
+}
+
+Console.WriteLine($"Сумма положительных эллементов массива = {positive_sum}");
+Console.WriteLine($"Сумма отрицательных эллементов массива = {negative_sum}");
